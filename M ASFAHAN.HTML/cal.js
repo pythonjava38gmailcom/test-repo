762b0a1daa2ca5e0ade115639964dc260e623e4c
huzaifa10-hue <syedhuzaifashah711@gmail.com>
2025-01-20 10:17:34 +0500

    uploding project

---
+++ M ASFAHAN.HTML/cal.js
@@ -0,0 +1,20 @@
+let displayvalue = '';
+
+function appendValue(value){
+    displayvalue += value;
+    document.getElementById('display').value = displayvalue;
+}
+
+function clearDisplay() {
+    displayvalue = '';
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        displayvalue = eval(displayvalue);
+        document.getElementById('display').value = displayvalue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
